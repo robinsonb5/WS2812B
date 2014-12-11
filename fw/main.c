e3ff7385bea767f2a8d6e91ca33dc3a30e889e0a
Alastair M. Robinson <robinsonb5@talk21.com>
2014-12-11 01:30:54 +0000

    Refactored effect code.

--- fw/main.c
+++ fw/main.c
@@ -18,8 +18,11 @@
 
 
 #define RIPPLE_SPRING 8
-#define RIPPLE_NODES 62
+#define RIPPLE_NODES 152
 #define RIPPLE_DAMP 15
+
+int lfsr;
+int rippledelay;
 
 struct ripple
 {
@@ -28,84 +31,102 @@ struct ripple
 };
 
 
-void ripple_clear(struct ripple *r)
+struct rgbripple
+{
+	int ro,go,bo;
+	struct ripple ripr;
+	struct ripple ripg;
+	struct ripple ripb;
+};
+
+
+struct LightEffect
+{
+	void (*Clear)(struct LightEffect *this,struct rgbripple *r);
+	void (*Iteration)(struct LightEffect *this,struct ripple *r);
+	void (*RGBIteration)(struct LightEffect *this,struct rgbripple *r);
+	void (*Update)(struct LightEffect *this,struct rgbripple *r);
+	void (*Send)(struct LightEffect *this,struct rgbripple *r);
+	void (*Add)(struct LightEffect *this,struct rgbripple *r);
+	int delaymask;
+};
+
+struct rgbripple ripples;
+
+
+
+// Generic routines applicable to most effects.
+
+void simple_upload(struct LightEffect *this,struct rgbripple *rgb)
+{
+	int i;
+	for(i=1;i<RIPPLE_NODES-1;++i)
+	{
+		int r=0,g=0,b=0;
+		int acc,base;
+
+		r=rgb->ripr.rv[i];
+		g=rgb->ripg.rv[i];
+		b=rgb->ripb.rv[i];
+
+		r>>=8;
+		g>>=8;
+		b>>=8;
+
+		r+=rgb->ro;
+		if(r>255) r=255;
+		g+=rgb->go;
+		if(g>255) g=255;
+		b+=rgb->bo;
+		if(b>255) b=255;
+
+		HW_LED(REG_RGBLED)=(r<<16)|(g<<8)|b;
+	}
+}
+
+
+void simple_rgbiteration(struct LightEffect *this,struct rgbripple *rgb)
+{
+	this->Iteration(this,&rgb->ripr);
+	this->Iteration(this,&rgb->ripg);
+	this->Iteration(this,&rgb->ripb);
+}
+
+
+void simple_clear(struct LightEffect *this,struct rgbripple *rgb)
 {
 	int i;
 	for(i=0;i<RIPPLE_NODES;++i)
 	{
-		r->rv[i]=0;
-		r->rvel[i]=0;
+		rgb->ripr.rv[i]=0;
+		rgb->ripr.rvel[i]=0;
+		rgb->ripg.rv[i]=0;
+		rgb->ripg.rvel[i]=0;
+		rgb->ripb.rv[i]=0;
+		rgb->ripb.rvel[i]=0;
 	}
+	rgb->ro=0;
+	rgb->go=0;
+	rgb->bo=0;
 }
 
 
-void ripple_iteration(struct ripple *r)
+void simple_update(struct LightEffect *this,struct rgbripple *rgb)
 {
-	int i,j;
-	int prev,prev2;
-	for(i=1;i<RIPPLE_NODES-1;++i)
+	int h;
+	int i;
+
+	if(rippledelay)
+		--rippledelay;
+	else
 	{
-		r->rv[i]+=r->rvel[i];
+		this->Add(this,rgb);
+		CYCLE_LFSR
+		rippledelay=lfsr&this->delaymask;
 	}
-	for(i=1;i<RIPPLE_NODES-1;++i)
-	{
-		int b=(0+2*r->rv[i-1]+3*r->rv[i]+2*r->rv[i+1])>>3;
-		int acc=(RIPPLE_SPRING*(b-r->rv[i]));
-		r->rvel[i]=(RIPPLE_DAMP*r->rvel[i])>>4;
-		r->rvel[i]+=acc>>4;
-	}
+	this->RGBIteration(this,rgb);
 }
 
-
-void scatter_iteration(struct ripple *r)
-{
-	int i,j;
-	int prev,prev2;
-	for(i=0;i<RIPPLE_NODES;++i)
-	{
-		if(r->rv[i]>128)
-			r->rv[i]-=128;
-		else
-			r->rv[i]=0;
-	}
-}
-
-
-void rain_iteration(struct ripple *r)
-{
-	int i,j;
-	int prev,prev2;
-	r->rv[0]=0;
-	for(i=RIPPLE_NODES;i>0;--i)
-	{
-		int t=r->rv[i-1];
-		if(r->rv[i]<(t-128))
-			r->rv[i]=t+128;
-		else if(r->rv[i]>512)
-			r->rv[i]-=512;
-		else
-			r->rv[i]=0;
-	}
-}
-
-
-void merge_iteration(struct ripple *r)
-{
-	int i,j;
-	int prev,prev2;
-	r->rv[0]=0;
-	r->rv[RIPPLE_NODES]=0;
-	for(i=1;i<RIPPLE_NODES-1;++i)
-	{
-		unsigned int j=61*r->rv[i]+r->rv[i-1]+r->rv[i+1];
-		r->rv[i]=j>>6;
-	}
-}
-
-
-struct ripple ripr,ripg,ripb;
-int lfsr;
-int rippledelay;
 
 
 // Add a random spike of colour.
@@ -163,77 +184,74 @@ void hsvtorgb(int h,int *rd,int *gn,int *bl)
 
 
 static int redt,greent,bluet;
-void add_hsv(int h,int v)
+void add_hsv(struct rgbripple *rgb,int h,int v)
 {
 	int r2;
 	CYCLE_LFSR
-	r2=1+(lfsr&63);
-	if(r2>59)
-		r2=59;
+	r2=(lfsr&255);
+	while(r2>150)
+		r2-=150;
+	++r2;
 	hsvtorgb(h,&redt,&greent,&bluet);
-	ripr.rv[r2]+=(v*redt)>>8;
-	ripg.rv[r2]+=(v*greent)>>8;
-	ripb.rv[r2]+=(v*bluet)>>8;
+	rgb->ripr.rv[r2]+=(v*redt)>>8;
+	rgb->ripg.rv[r2]+=(v*greent)>>8;
+	rgb->ripb.rv[r2]+=(v*bluet)>>8;
 }
 
 
-void add_rgb()
+void add_rgb(struct LightEffect *this,struct rgbripple *rgb)
 {
 	int h;
 	h=lfsr&0x7ffff;
-	add_hsv(h,256);
+	add_hsv(rgb,h,256);
 }
 
 
-void ripple_update()
+
+// Colour ripple effect
+
+void ripple_upload(struct LightEffect *this,struct rgbripple *rgb)
 {
 	int i;
-
-	if(rippledelay)
-		--rippledelay;
-	else
-	{
-		add_rgb();
-		rippledelay=lfsr&255;
-	}
-	for(i=1;i<61;++i)
+	for(i=1;i<RIPPLE_NODES-1;++i)
 	{
 		int r=0,g=0,b=0;
 		int acc,base;
 
-		if(ripr.rvel[i]<0)
-			r=-ripr.rvel[i];
-		else
-			r=ripr.rvel[i];
+		if(rgb->ripr.rvel[i]<0)
 
-		if(ripg.rvel[i]<0)
-			g=-ripg.rvel[i];
+			r=-rgb->ripr.rvel[i];
 		else
-			g=ripg.rvel[i];
+			r=rgb->ripr.rvel[i];
 
-		if(ripb.rvel[i]<0)
-			b=-ripb.rvel[i];
+		if(rgb->ripg.rvel[i]<0)
+			g=-rgb->ripg.rvel[i];
 		else
-			b=ripb.rvel[i];
+			g=rgb->ripg.rvel[i];
 
-		base=(0+2*ripr.rv[i-1]+3*ripr.rv[i]+2*ripr.rv[i+1])>>3;
-		acc=(RIPPLE_SPRING*(base-ripr.rv[i]));
+		if(rgb->ripb.rvel[i]<0)
+			b=-rgb->ripb.rvel[i];
+		else
+			b=rgb->ripb.rvel[i];
+
+		base=(0+2*rgb->ripr.rv[i-1]+3*rgb->ripr.rv[i]+2*rgb->ripr.rv[i+1])>>3;
+		acc=(RIPPLE_SPRING*(base-rgb->ripr.rv[i]));
 
 		if(acc<0)
 			r-=acc;
 		else
 			r+=acc;
 
-		base=(0+2*ripg.rv[i-1]+3*ripg.rv[i]+2*ripg.rv[i+1])>>3;
-		acc=(RIPPLE_SPRING*(base-ripg.rv[i]));
+		base=(0+2*rgb->ripg.rv[i-1]+3*rgb->ripg.rv[i]+2*rgb->ripg.rv[i+1])>>3;
+		acc=(RIPPLE_SPRING*(base-rgb->ripg.rv[i]));
 
 		if(acc<0)
 			g-=acc;
 		else
 			g+=acc;
 
-		base=(0+2*ripb.rv[i-1]+3*ripb.rv[i]+2*ripb.rv[i+1])>>3;
-		acc=(RIPPLE_SPRING*(base-ripb.rv[i]));
+		base=(0+2*rgb->ripb.rv[i-1]+3*rgb->ripb.rv[i]+2*rgb->ripb.rv[i+1])>>3;
+		acc=(RIPPLE_SPRING*(base-rgb->ripb.rv[i]));
 
 		if(acc<0)
 			b-=acc;
@@ -249,33 +267,237 @@ void ripple_update()
 		b&=255;
 		HW_LED(REG_RGBLED)=(r<<16)|(g<<8)|b;
 	}
-	ripple_iteration(&ripr);
-	ripple_iteration(&ripg);
-	ripple_iteration(&ripb);
 }
 
 
-void upload_simple(int ro,int go,int bo)
+void ripple_iteration(struct LightEffect *this,struct ripple *r)
+{
+	int i,j;
+	int prev,prev2;
+	for(i=1;i<RIPPLE_NODES-1;++i)
+	{
+		r->rv[i]+=r->rvel[i];
+	}
+	for(i=1;i<RIPPLE_NODES-1;++i)
+	{
+		int b=(0+2*r->rv[i-1]+3*r->rv[i]+2*r->rv[i+1])>>3;
+		int acc=(RIPPLE_SPRING*(b-r->rv[i]));
+		r->rvel[i]=(RIPPLE_DAMP*r->rvel[i])>>4;
+		r->rvel[i]+=acc>>4;
+	}
+}
+
+
+const struct LightEffect RippleEffect=
+{
+	simple_clear,
+	ripple_iteration,
+	simple_rgbiteration,
+	simple_update,
+	ripple_upload,
+	add_rgb,
+	255
+};
+
+
+
+// Scatter effect
+
+void scatter_iteration(struct LightEffect *this,struct ripple *r)
+{
+	int i,j;
+	int prev,prev2;
+	for(i=0;i<RIPPLE_NODES;++i)
+	{
+		if(r->rv[i]>128)
+			r->rv[i]-=128;
+		else
+			r->rv[i]=0;
+	}
+}
+
+
+const struct LightEffect ScatterEffect=
+{
+	simple_clear,
+	scatter_iteration,
+	simple_rgbiteration,
+	simple_update,
+	simple_upload,
+	add_rgb,
+	63
+};
+
+
+
+// Rain effect
+
+void rain_iteration(struct LightEffect *this,struct ripple *r)
+{
+	int i,j;
+	int prev,prev2;
+	r->rv[0]=0;
+	for(i=RIPPLE_NODES;i>0;--i)
+	{
+		int t=r->rv[i-1];
+		if(r->rv[i]<(t-128))
+			r->rv[i]=t+128;
+		else if(r->rv[i]>512)
+			r->rv[i]-=512;
+		else
+			r->rv[i]=0;
+	}
+}
+
+
+void rain_add(struct LightEffect *this,struct rgbripple *rgb)
+{
+	int h=lfsr&0x1ffff;
+	add_hsv(rgb,h+0x24000,lfsr&255);
+}
+
+
+const struct LightEffect RainEffect=
+{
+	simple_clear,
+	rain_iteration,
+	simple_rgbiteration,
+	simple_update,
+	simple_upload,
+	rain_add,
+	63
+};
+
+
+
+// Merge effect
+
+
+void merge_iteration(struct LightEffect *this,struct ripple *r)
+{
+	int i,j;
+	int prev,prev2;
+	r->rv[0]=0;
+	r->rv[RIPPLE_NODES]=0;
+	for(i=1;i<RIPPLE_NODES-1;++i)
+	{
+		unsigned int j=61*r->rv[i]+r->rv[i-1]+r->rv[i+1];
+		r->rv[i]=j>>6;
+	}
+}
+
+const struct LightEffect MergeEffect=
+{
+	simple_clear,
+	merge_iteration,
+	simple_rgbiteration,
+	simple_update,
+	simple_upload,
+	add_rgb,
+	63
+};
+
+
+// Fire effect
+
+
+void fire_background(struct ripple *r)
 {
 	int i;
-	for(i=1;i<61;++i)
+	unsigned int j;
+	unsigned int h1,h2,h3;
+	j=r->rvel[0]+r->rvel[1]+r->rvel[2]+r->rvel[3];
+	h1=r->rvel[0];
+	h2=r->rvel[1];
+	for(i=2;i<RIPPLE_NODES-2;++i)
+	{
+		h3=r->rvel[i];
+		j+=r->rvel[i+2];
+		r->rvel[i]=(j*52)>>8;
+		j-=h1;
+		h1=h2;
+		h2=h3;
+	}	
+}
+
+
+void fire_clear(struct LightEffect *this,struct rgbripple *rgb)
+{
+	int i,j,h;
+	simple_clear(this,rgb);
+
+	// Set random background colours
+	for(i=0;i<RIPPLE_NODES;++i)
+	{
+		h=lfsr&0x3fff;
+		hsvtorgb(h+0x1fff,&redt,&greent,&bluet);
+		CYCLE_LFSR
+		j=64+lfsr&15;
+		rgb->ripr.rvel[i]=(redt*j)>>16;
+		rgb->ripg.rvel[i]=(greent*j)>>16;
+		rgb->ripb.rvel[i]=(bluet*j)>>16;
+	}
+	fire_background(&rgb->ripr);
+	fire_background(&rgb->ripg);
+	fire_background(&rgb->ripb);
+}
+
+
+void fire_rgbiteration(struct LightEffect *this,struct rgbripple *rgb)
+{
+	int i,h;
+	this->Iteration(this,&rgb->ripr);
+	this->Iteration(this,&rgb->ripg);
+	this->Iteration(this,&rgb->ripb);
+
+	// Background colour
+	h=lfsr&0x3fff;
+	hsvtorgb(h+0x1fff,&redt,&greent,&bluet);
+	CYCLE_LFSR
+	i=64+lfsr&15;
+	// Pick one node at random each frame.
+	CYCLE_LFSR;
+	h=lfsr&255;
+	while(h>RIPPLE_NODES-5)
+		h-=RIPPLE_NODES-5;
+	rgb->ripr.rvel[h+2]=(redt*i)>>16;
+	rgb->ripg.rvel[h+2]=(greent*i)>>16;
+	rgb->ripb.rvel[h+2]=(bluet*i)>>16;
+	fire_background(&rgb->ripr);
+	fire_background(&rgb->ripg);
+	fire_background(&rgb->ripb);
+}
+
+void fire_add(struct LightEffect *this,struct rgbripple *rgb)
+{
+	int h=lfsr&0x7fff;
+	add_hsv(rgb,h+0x3fff,lfsr&255);
+	CYCLE_LFSR
+	rippledelay=lfsr&127;
+}
+
+
+void fire_upload(struct LightEffect *this,struct rgbripple *rgb)
+{
+	int i;
+	for(i=1;i<RIPPLE_NODES-1;++i)
 	{
 		int r=0,g=0,b=0;
 		int acc,base;
 
-		r=ripr.rv[i];
-		g=ripg.rv[i];
-		b=ripb.rv[i];
+		r=rgb->ripr.rv[i];
+		g=rgb->ripg.rv[i];
+		b=rgb->ripb.rv[i];
 
 		r>>=8;
 		g>>=8;
 		b>>=8;
 
-		r+=ro;
+		r+=rgb->ripr.rvel[i];
 		if(r>255) r=255;
-		g+=go;
+		g+=rgb->ripg.rvel[i];
 		if(g>255) g=255;
-		b+=bo;
+		b+=rgb->ripb.rvel[i];;
 		if(b>255) b=255;
 
 		HW_LED(REG_RGBLED)=(r<<16)|(g<<8)|b;
@@ -283,126 +505,82 @@ void upload_simple(int ro,int go,int bo)
 }
 
 
-void scatter_update()
+const struct LightEffect FireEffect=
 {
-	int i;
+	fire_clear,
+	merge_iteration,
+	fire_rgbiteration,
+	simple_update,
+	fire_upload,
+	fire_add,
+	63
+};
 
-	if(rippledelay)
-		--rippledelay;
-	else
-	{
-		add_rgb();
-		rippledelay=lfsr&63;
-	}
-	upload_simple(0,0,0);
-	scatter_iteration(&ripr);
-	scatter_iteration(&ripg);
-	scatter_iteration(&ripb);
-}
 
-void merge_update()
+// Fire and Ice
+
+
+const struct LightEffect FireAndIceEffect=
 {
-	int i;
-
-	if(rippledelay)
-		--rippledelay;
-	else
-	{
-		add_rgb();
-		rippledelay=lfsr&255;
-	}
-	upload_simple(0,0,0);
-	merge_iteration(&ripr);
-	merge_iteration(&ripg);
-	merge_iteration(&ripb);
-}
-
-
-void rain_update()
-{
-	int i,h;
-
-	if(rippledelay)
-		--rippledelay;
-	else
-	{
-		h=lfsr&0x1ffff;
-		add_hsv(h+0x20000,lfsr&255);
-		CYCLE_LFSR
-		rippledelay=lfsr&63;
-	}
-	upload_simple(0,0,0);
-	rain_iteration(&ripr);
-	rain_iteration(&ripg);
-	rain_iteration(&ripb);
-}
-
-
-void fire_update()
-{
-	int h;
-	int i;
-
-	if(rippledelay)
-		--rippledelay;
-	else
-	{
-		h=lfsr&0x7fff;
-		add_hsv(h+0x3fff,lfsr&255);
-		CYCLE_LFSR
-		rippledelay=lfsr&127;
-	}
-	h=lfsr&0x3fff;
-	hsvtorgb(h+0x1fff,&redt,&greent,&bluet);
-	CYCLE_LFSR
-	i=60+lfsr&3;
-	upload_simple((redt*i)>>16,(greent*i)>>16,(bluet*i)>>16);
-	merge_iteration(&ripr);
-	merge_iteration(&ripg);
-	merge_iteration(&ripb);
-}
+	fire_clear,
+	merge_iteration,
+	fire_rgbiteration,
+	simple_update,
+	fire_upload,
+	rain_add,
+	63
+};
 
 
 int main(int argc, char **argv)
 {
 	int c,i;
+	int prevsw;
 
 	rippledelay=1;
-	ripple_clear(&ripr);
-	ripple_clear(&ripg);
-	ripple_clear(&ripb);
+	struct LightEffect *effect=&RippleEffect;
+	effect->Clear(effect,&ripples);
 
 	lfsr=LFSR_SEED;
 	c=0;
 
 	CYCLE_LFSR
-
+	prevsw=-1;
 	while(1)
 	{
 		int sw=HW_IO(REG_SWITCHES);
-
-		switch(sw>>4)
+		if(sw!=prevsw)
 		{
-			case 0:
-				ripple_update();
-				break;
-			case 1:
-				scatter_update();
-				break;
-			case 2:
-				merge_update();
-				break;
-			case 3:
-				fire_update();
-				break;
-			case 4:
-				rain_update();
-				break;
-			default:
-				break;
+			switch(sw>>4)
+			{
+				case 0:
+					effect=&RippleEffect;
+					break;
+				case 1:
+					effect=&ScatterEffect;
+					break;
+				case 2:
+					effect=&MergeEffect;
+					break;
+				case 3:
+					effect=&FireEffect;
+					break;
+				case 4:
+					effect=&RainEffect;
+					break;
+				case 5:
+					effect=&FireAndIceEffect;
+					break;
+				default:
+					break;
+			}
+			prevsw=sw;
+			effect->Clear(effect,&ripples);
 		}
+		effect->Update(effect,&ripples);
+		effect->Send(effect,&ripples);
 
-		for(i=0;i<7500;++i)
+		for(i=0;i<2500;++i)
 			c=HW_UART(REG_UART);			
 	}
 
